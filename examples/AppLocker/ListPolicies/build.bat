4bef41247af2fe1bd7f7792ab78ecd9fc688aa3d
Paul Laîné (@am0nsec) <20334678+am0nsec@users.noreply.github.com>
2020-08-18 20:22:46 +0100

    Update build.bat

--- examples/AppLocker/ListPolicies/build.bat
+++ examples/AppLocker/ListPolicies/build.bat
@@ -1,4 +1,10 @@
 @echo off
 
+SET OldInclude=%INCLUDE%
+SET IncludeDir=%cd%
+SET INCLUDE=%INCLUDE%;%IncludeDir%\..\..\..\include\
+
 ml64.exe /c /Zi /Fo"ListPolicies.obj" /W3 /errorReport:prompt ListPolicies.asm
 link.exe /ERRORREPORT:PROMPT /INCREMENTAL:NO /DEBUG /SUBSYSTEM:CONSOLE /OPT:NOREF /OPT:NOICF /ENTRY:"ListPolicies" /DYNAMICBASE /NXCOMPAT /MACHINE:X64 /SAFESEH:NO ListPolicies.obj kernel32.lib ole32.lib
+
+SET INCLUDE=%OldInclude%
